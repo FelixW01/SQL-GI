495dd25f7da9919e44ecc912655cfaf843106438
Felix W <felixwillem01@yahoo.com>
2025-01-05 16:16:53 -0500

    worked on adding JOINS practice problems skeleton

---
+++ Felix_Willem_MySQL_JoinsProblems1.sql
@@ -0,0 +1,33 @@
+-- Felix WIllem GI SQL-JOINS Week 14
+
+-- 1. Using customer and salesman tables, write a SQL query to find the salesperson and customer who reside in the same city. Return Salesman, cust_name and city
+
+
+-- 2. Using the order and customer tables, write a SQL query to find those orders where the order amount exists between 500 and 2000. Return ord_no, purch_amt, cust_name, city.
+
+
+-- 3. Using the customer, and salesmans tables write a SQL query to find the salesperson(s) and the customer(s) he represents. Return Customer Name, city, Salesman, commission.
+
+
+-- 4. Using the customer and salesman tables write a SQL query to find salespeople who received commissions of more than 12 percent from the company. Return Customer Name, customer city, Salesman, commission.
+
+
+-- 5. Using the customer and salesman tables write a SQL query to locate those salespeople who do not live in the same city where their customers live and have received a commission of more than 12% from the company. Return Customer Name, customer city, Salesman, salesman city, commission.
+
+
+-- 6. Using all three tables write a SQL query to find the details of an order. Return ord_no, ord_date, purch_amt, Customer Name, grade, Salesman, commission.
+
+
+-- 7. Using all three tables, write a SQL statement to join the tables salesman, customer and orders so that the same column of each table appears once and only the relational rows are returned.
+
+
+-- 8. Using the customer and salesman tables write a SQL query to display the customer name, customer city, grade, salesman, salesman city. The results should be sorted by ascending customer_id.
+
+
+-- 9. Using the customer and salesman tables write a SQL query to find those customers with a grade less than 300. Return cust_name, customer city, grade, Salesman, salesmancity. The result should be ordered by ascending customer_id.
+
+
+-- 10. Using the customer and salesman tables, Write a SQL statement to make a report with customer name, city, order number, order date, and order amount in ascending order according to the order date to determine whether any of the existing customers have placed an order or not. 
+
+
+-- 11. Using all three tables, write an SQL statement to generate a report with customer name, city, order number, order date, order amount, salesperson name, and commission to determine if any of the existing customers have not placed orders or if they have placed orders through their salesman or by themselves.
